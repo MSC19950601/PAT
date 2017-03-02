4ab9a38840dcc166a56523e0643a4e4c57a4fd78
Ellery Queen <yhmsc007@hotmail.com>
2017-03-02 13:35:01 +0800

    20170302

---
+++ A/1004.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+const int N = 110;
+
+vector<int> tree[N];
+int leaf[N] = {0};
+int max_h = 1;
+
+void DFS(int index, int h)
+{
+    max_h = max(h, max_h);
+    if (tree[index].size() == 0) {
+        leaf[h]++;
+        return;
+    }
+    for(int i = 0; i < tree[ind
+    ])
+}
+
+
+int main(){
+    int n,m;
+    int parent, childNum, child;
+    cin >> n >> m;
+    for (int i = 0; i < m; i++) {
+        cin >> parent >> childNum;
+        for (int j = 0; j < childNum; j++) {
+            cin >> child;
+            tree[parent].push_back(child);
+        }
+    }
+    DFS(1,1);
+
+}
