6f530dfbb68eacb556d488a1ef0b5fa36d987032
Ellery Queen <yhmsc007@hotmail.com>
2017-03-05 00:29:34 +0800

    2017PAT春季甲级3

---
+++ A/1126.cpp
@@ -0,0 +1,78 @@
+// In graph theory, an Eulerian path is a path in a graph which visits every edge exactly once. Similarly, an Eulerian circuit is an Eulerian path which starts and ends on the same vertex. They were first discussed by Leonhard Euler while solving the famous Seven Bridges of Konigsberg problem in 1736. It has been proven that connected graphs with all vertices of even degree have an Eulerian circuit, and such graphs are called Eulerian. If there are exactly two vertices of odd degree, all Eulerian paths start at one of them and end at the other. A graph that has an Eulerian path but not an Eulerian circuit is called semi-Eulerian. (Cited from https://en.wikipedia.org/wiki/Eulerian_path)
+
+// Given an undirected graph, you are supposed to tell if it is Eulerian, semi-Eulerian, or non-Eulerian.
+
+// Input Specification:
+
+// Each input file contains one test case. Each case starts with a line containing 2 numbers N (<= 500), and M, which are the total number of vertices, and the number of edges, respectively. Then M lines follow, each describes an edge by giving the two ends of the edge (the vertices are numbered from 1 to N).
+
+// Output Specification:
+
+// For each test case, first print in a line the degrees of the vertices in ascending order of their indices. Then in the next line print your conclusion about the graph -- either "Eulerian", "Semi-Eulerian", or "Non-Eulerian". Note that all the numbers in the first line must be separated by exactly 1 space, and there must be no extra space at the beginning or the end of the line.
+
+//图论 欧拉回路。这个坑留着再填
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+const int maxn = 505;
+
+int arc[maxn][maxn] = {};
+int n, m;
+int degree[maxn] = {};
+int visited[maxn] = {};
+
+void dfs(int index)
+{
+	for (int t = 1; t <= n; t++)
+		if (!visited[t] && arc[index][t] != 0)
+		{
+			visited[t] = 1;
+			dfs(t);
+		}
+}
+
+int main()
+{
+	std::cin >> n >> m;
+
+	while (m--)
+	{
+		int a, b;
+		std::cin >> a >> b;
+		degree[a]++;
+		degree[b]++;
+		arc[a][b] = arc[b][a] = 1;
+	}
+
+	int oddnum = 0;
+	int cnt = 0;
+	cout << degree[1];
+	if (degree[1] % 2) oddnum++;
+	for (int i = 2; i <= n; i++)
+	{
+		cout << " ";
+		if (degree[i])
+		{
+			oddnum++;
+		}
+	}
+	cout << endl;
+
+	for (int i = 1; i <= n; i++)
+	{
+		if(!visited[i])
+		{
+			if (++cnt == 2)
+				break;
+			visited[i] = 1;
+			dfs(i);
+		}
+	}
+	if (cnt == 2) { cout << "Non-Eulerian" << endl; return 0; }
+	if (oddnum == 0) cout << "Eulerian" << endl;
+	else if (oddnum == 2) cout << "Semi-Eulerian" << endl;
+	else cout << "Non-Eulerian" << endl;
+	return 0;
+}
